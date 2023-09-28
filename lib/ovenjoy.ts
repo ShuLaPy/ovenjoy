d55db646e00010c88a4099a9ad8a04d2609babbd
Shubham Lad <shubham.lad@softway.com>
2023-09-28 19:19:45 +0530

    refactor: refactored ovenjoy file code

--- lib/ovenjoy.ts
+++ lib/ovenjoy.ts
@@ -1,7 +1,10 @@
+import type { Serve, Server } from 'bun';
+
 class OvenJoyServer {
   // singleton OvenJoy Server
   private static server?: OvenJoyServer;
 
+  // Make sure we return same instance when called multiple times
   public static getInstance(): OvenJoyServer {
     if (!OvenJoyServer.server) {
       OvenJoyServer.server = new OvenJoyServer();
@@ -10,7 +13,18 @@ class OvenJoyServer {
     return OvenJoyServer.server;
   }
 
-  listen(port: string | number, cb?: () => void) {
+  /**
+   * Start Bun server and listen for connections.
+   *
+   * A node `Bun.Server` is returned.
+   *
+   * @param {string | number} port
+   * @param {Function} cb or callback
+   * @return {Server}
+   * @public
+   */
+
+  listen(port: string | number, cb?: () => void): Server {
     try {
       const server = Bun.serve(this.getSettings(port));
       cb?.call(null);
@@ -21,27 +35,52 @@ class OvenJoyServer {
     }
   }
 
-  private getSettings(port: string | number) {
+  /**
+   * Generate and return Bun Server options.
+   *
+   * @param {string | number} port
+   * @return {Serve}
+   * @private
+   */
+
+  private getSettings(port: string | number): Serve {
     const settings = {
       port: port,
       development: process.env.NODE_ENV !== 'production',
-      async fetch(req: Request) {
-        const headers = { 'Content-Type': 'application/json' };
-        return new Response(JSON.stringify({ data: 'Hello World' }), {
-          headers,
-        });
-      },
-      error(error: any) {
-        return new Response(
-          'An error occurred! Please check the server logs.',
-          {
-            status: 500,
-          }
-        );
-      },
+      fetch: this.fetch,
+      error: this.outerErrorHandler,
     };
     return settings;
   }
+
+  /**
+   * Handle all incomming requests.
+   *
+   * @param {Request} req
+   * @return {Response}
+   * @private
+   */
+
+  private fetch = async (req: Request): Promise<Response> => {
+    const headers = { 'Content-Type': 'application/json' };
+    return new Response(JSON.stringify({ data: 'Hello World' }), {
+      headers,
+    });
+  };
+
+  /**
+   * Handle request processing errors
+   *
+   * @param {Error} error
+   * @return {Response}
+   * @private
+   */
+
+  private outerErrorHandler = (error: Error): Response =>
+    new Response(error.message, {
+      // @ts-ignore
+      status: error?.status ?? 500,
+    });
 }
 
 export default OvenJoyServer;
